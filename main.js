f1add6b9fe0014bc7fb705d0cce244d9606f8cae
opigo joseph <opigojoseph4@gmail.com>
2020-11-07 19:26:37 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function openslidemenu(){
+    document.getElementById('side-menu').style.width='180px';
+    document.getElementById('main').style.marginRight='0';
+}
+function closeslidemenu(){
+    document.getElementById('side-menu').style.width='0';
+    document.getElementById('main').style.marginLeft='0';
+}
+
+function main(){
+    document.getElementById('side-menu').style.width ='0';
+}
+
+document.querySelector('.p1').textContent = skills
